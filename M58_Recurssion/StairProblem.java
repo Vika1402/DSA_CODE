b4b66a371bbfd7053c6edc8de12f07f5983caf3e
Vikas Kumar <89458526+Vika1402@users.noreply.github.com>
2024-08-06 04:15:26 +0530

    Add files via upload
    add fibonaccia and stair problem

---
+++ M58_Recurssion/StairProblem.java
@@ -0,0 +1,13 @@
+public class StairProblem {
+public static int solve(int n){
+  if(n==1 || n==2){
+    return n;
+  }
+  int result=solve(n-1)+solve(n-2);
+  return result;
+}
+  public static void main(String[] args) {
+    int n=7;
+    System.out.println(solve(n));
+  }
+}
